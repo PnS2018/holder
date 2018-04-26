b40583d7526c15ec7e9475ffae80c9ee99332ea4
jbinz <jbinz@student.ethz.ch>
2018-04-26 15:10:21 +0000

    one script

--- Model_trainer.py
+++ Model_trainer.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from Classification_model import Classification_model as Cm
+from Classification_model2 import Classification_model as Cm2
 
 
 # Script for training different models
@@ -9,3 +10,7 @@ from Classification_model import Classification_model as Cm
 cm = Cm("conv_net_not_real_time")
 cm.load_model()
 cm.train_model()
+
+cm2 = Cm2("conv_net_real_time")
+cm2.load_model()
+cm2.train_model()
